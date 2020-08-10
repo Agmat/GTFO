79cd72f712c3e4af004d854de1dd40969f0f53c6
Gabriel Cabanes <gcabanes@meilleursagents.com>
2020-08-10 12:57:54 +0200

    feat(inject): properly replace window notification object

--- src/notification.js
+++ src/notification.js
@@ -1,1 +1,18 @@
-console.log(window);
+(function() {
+  const script = () => {
+    const OldNotify = window.Notification;
+    const newNotify = (title, opt) => {
+      console.log('Notification incoming');
+      return new OldNotify(title, opt);
+    };
+    window.Notification = newNotify;
+  };
+
+  const inject = (fn) => {
+    const script = document.createElement('script');
+    script.text = `(${fn.toString()})();`;
+    document.documentElement.appendChild(script);
+  };
+
+  inject(script);
+})();
